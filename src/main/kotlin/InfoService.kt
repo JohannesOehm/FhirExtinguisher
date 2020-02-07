817cfa792cc028545ad5ed84302fc4724da64ea8
Johannes Oehm <johannes.oehm@uni-muenster.de>
2020-02-07 10:20:23 +0100

    Added InfoService

---
+++ src/main/kotlin/InfoService.kt
@@ -0,0 +1,14 @@
+import fi.iki.elonen.NanoHTTPD
+import mu.KotlinLogging
+
+private val log = KotlinLogging.logger {}
+
+class InfoService(private val instanceConfiguration: InstanceConfiguration) {
+    fun serve(req: NanoHTTPD.IHTTPSession): NanoHTTPD.Response {
+        val result = """{
+            "server": "${instanceConfiguration.fhirServerUrl}",
+            "version" "${instanceConfiguration.fhirVersion.version.version.fhirVersionString}"
+        }"""
+        return NanoHTTPD.newFixedLengthResponse(NanoHTTPD.Response.Status.OK, "application/json", result)
+    }
+}
--- src/main/kotlin/MyRouters.kt
+++ src/main/kotlin/MyRouters.kt
@@ -10,12 +10,15 @@ class MyRouters(portnumber: Int, private val instanceConfiguration: InstanceConf
     var staticPageHandler = StaticPageHandler()
 
     var redirectService = RedirectService(instanceConfiguration.fhirServerUrl)
+    var infoService = InfoService(instanceConfiguration)
 
     override fun serve(session: IHTTPSession): Response {
         if (session.uri.startsWith("/fhir/")) {
             return fhirExtinguisher.serve(session)
         } else if (session.uri.startsWith("/redirect/")) {
             return redirectService.serve(session)
+        } else if (session.uri.startsWith("/info")) {
+            return infoService.serve(session)
         } else {
             return staticPageHandler.serve(session)
         }
