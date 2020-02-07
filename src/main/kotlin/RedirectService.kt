abb91eee832256b9d63eb3c3d73324d17f24b911
Johannes Oehm <johannes.oehm@uni-muenster.de>
2020-02-07 10:20:38 +0100

    Improved RedirectService

--- src/main/kotlin/RedirectService.kt
+++ src/main/kotlin/RedirectService.kt
@@ -1,5 +1,6 @@
 import fi.iki.elonen.NanoHTTPD
 import mu.KotlinLogging
+import org.apache.http.StatusLine
 import org.apache.http.client.methods.HttpGet
 import org.apache.http.impl.client.HttpClients
 import java.io.ByteArrayOutputStream
@@ -12,12 +13,26 @@ class RedirectService(private val urlPrefix: String) {
         val client = HttpClients.createDefault()
         val targetUrl = urlPrefix + "/" + req.uri.drop("/redirect/".length)
 
-        log.info { "targetUrl = $targetUrl" }
+        log.info { "redirecting request to targetUrl = $targetUrl" }
 
         val response = client.execute(HttpGet(targetUrl))
-        val myResponse = NanoHTTPD.newFixedLengthResponse(inputStreamToString(response.entity.content))
+
+        val contentType = response.getFirstHeader("Content-Type").value
+        val status = convertStatus(response.statusLine)
+        val content = inputStreamToString(response.entity.content)
         response.close()
-        return myResponse
+
+        return NanoHTTPD.newFixedLengthResponse(status, contentType, content)
+    }
+
+    private fun convertStatus(statusLine: StatusLine): NanoHTTPD.Response.IStatus {
+        val statusCode = statusLine.statusCode
+        val reasonPhrase = statusLine.reasonPhrase
+        return NanoHTTPD.Response.Status.values().find { it.requestStatus == statusCode } ?: object :
+            NanoHTTPD.Response.IStatus {
+            override fun getRequestStatus() = statusCode
+            override fun getDescription() = reasonPhrase
+        }
     }
 
     private fun inputStreamToString(inputStream: InputStream): String {
