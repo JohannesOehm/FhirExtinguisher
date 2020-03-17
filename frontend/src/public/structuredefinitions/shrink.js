fbda8f739c4a7b7dc3b4d6f749470f7d85469383
Johannes Oehm <johannes.oehm@student.uni-luebeck.de>
2020-03-17 16:21:22 +0100

    Shrinking StructureDefinition bundles file sizes

---
+++ frontend/src/public/structuredefinitions/shrink.js
@@ -0,0 +1,36 @@
+/**
+ *  The purpose of this script is to remove the unnecessary documentation and unimportant information from the FHIR StructureDefinition,
+ *  to speed up performance in browser and decrease the FhirExtinguisher executable size.
+ */
+
+let fs = require('fs');
+let path = require("path");
+
+function removeUnncessaryFields(bundle) {
+    for(let entry of bundle.entry) {
+        delete entry.resource.mapping;
+        delete entry.resource.differential;
+        delete entry.resource.description;
+        delete entry.resource.contact;
+        delete entry.resource.text;
+        delete entry.resource.meta;
+        if(entry.resource.snapshot) {
+            for (let element of entry.resource.snapshot.element) {
+                delete element.definition;
+                delete element.short;
+                delete element.constraint;
+                delete element.comment;
+                delete element.mapping;
+            }
+        }
+    }
+}
+
+for (let fhirVersion of ["stu3", "r4"]) {
+    for (let fileName of ["profiles-resources.json", "profiles-types.json"]) {
+        let filename = path.join(__dirname, fhirVersion, fileName);
+        let data = JSON.parse(fs.readFileSync(filename));
+        removeUnncessaryFields(data);
+        fs.writeFileSync(filename, JSON.stringify(data))
+    }
+}
