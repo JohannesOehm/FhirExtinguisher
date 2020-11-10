c5359d4398d95b701e84af994934ebc8cdfebf60
Johannes Oehm <johannes.oehm@uni-muenster.de>
2020-11-10 16:38:20 +0100

    added button to toggle stringify behaviour

--- frontend/src/app/vue/dialog-test-fhirpath.vue
+++ frontend/src/app/vue/dialog-test-fhirpath.vue
@@ -1,15 +1,26 @@
 <template>
   <b-modal id="modal-test-fhirpath" title="Test FHIRPath" size="xl" hide-footer="true">
-    <div class="form-group">
-      <div class="input-group">
-        <div class="input-group-prepend">
-          <label class="input-group-text" for="fhirpathString">FHIRPath:</label>
+    <div class="form-row align-items-center">
+      <div class="col-10">
+        <div class="input-group">
+          <div class="input-group-prepend">
+            <label class="input-group-text" for="fhirpathString">FHIRPath:</label>
+          </div>
+          <input type="text" class="form-control" id="fhirpathString" v-model="fhirpath"
+                 @keyup="evaluateExpression(fhirpath, resource, stringify)">
         </div>
-        <input type="text" class="form-control" id="fhirpathString" v-model="fhirpath"
-               @keyup="evaluateExpression(fhirpath, resource)">
+      </div>
+      <div class="col-auto">
+        <div class="form-check mb-2">
+          <input class="form-check-input" type="checkbox" id="stringify" v-model="stringify"
+                 @="evaluateExpression(fhirpath, resource, stringify)">
+          <label class="form-check-label" for="stringify">
+            stringify()
+          </label>
+        </div>
       </div>
     </div>
-    <div id="resultView">
+    <div class="form-row" style="padding-top: 10px;">
       <ul>
         <li v-for="t in text">
           <pre>{{ t }}</pre>
@@ -39,22 +50,23 @@ import * as _ from "lodash";
 
 export default {
   name: "DialogTestFhirpath",
-  data: function (): { fhirpath: string, text: string[], error: string | null } {
-    return {fhirpath: "stringify($this)", text: [], error: null}
+  data: function (): { fhirpath: string, text: string[], error: string | null, stringify: boolean } {
+    return {fhirpath: "$this", text: [], error: null, stringify: true}
   },
   mounted() {
     this.$root.$on('bv::modal::shown', (bvEvent: any, modalId: any) => {
       if (modalId === "modal-test-fhirpath") {
-        this.evaluateExpression(this.fhirpath, this.resource)
+        this.evaluateExpression(this.fhirpath, this.resource, this.stringify)
       }
     })
   },
   props: ["resource"],
   methods: {
-    evaluateExpression: _.debounce(async function (fhirpath: string, resource: string): Promise<void> {
+    evaluateExpression: _.debounce(async function (fhirpath: string, resource: string, stringify: boolean): Promise<void> {
       //TODO: Use debounce!!
       try {
-        let response = await fetch("/fhirPath?expr=" + encodeURIComponent(fhirpath), {
+        let expr = stringify ? `stringify(${fhirpath})` : fhirpath;
+        let response = await fetch("/fhirPath?expr=" + encodeURIComponent(expr), {
           method: "POST",
           body: resource,
           headers: {
